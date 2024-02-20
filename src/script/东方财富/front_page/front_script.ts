ee2f08a5a57ab8c47ef8aa33bc42a0bce5f741af
liyuelin.kyssion <liyuelin.kyssion@bytedance.com>
2024-02-20 20:16:12 +0800

    feat: add puppeteer

---
+++ src/script/东方财富/front_page/front_script.ts
@@ -0,0 +1,36 @@
+import puppeteer, {Puppeteer} from "puppeteer";
+
+const demo = async() => {
+    // 开启一个浏览器进程
+    const browser = await puppeteer.launch({
+            headless: false,
+        }
+    )
+    // 开启浏览器窗口
+    const defaultContext = browser.defaultBrowserContext()
+    // const newContextA = await browser.createBrowserContext()
+    // const newContextB = await browser.createBrowserContext()
+    // 开启新的标签页 - 默认的的context 默认就有一个pages
+    const defaultPage = (await defaultContext.pages())[0]
+    // 如果有需要可以再这里再添加
+    // const pageA = await newContextA.newPage()
+    // const pageB = await newContextB.newPage()
+
+    // goto 方法跳转到百度中
+    await defaultPage.goto("https://www.bilibili.com")
+
+    // -- 这里是浏览器页面中的操作
+    // 操作...
+    setTimeout(async ()=>{
+        // 关闭页面
+        await defaultPage.close()
+        // 关闭标签
+        await defaultPage.close()
+        // 关闭整个浏览器
+        await browser.close()
+    },10000000)
+}
+
+demo()
+
+
