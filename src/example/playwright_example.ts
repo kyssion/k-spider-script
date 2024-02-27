72497a9538e85000dd66ae273a4600541288ea6a
liyuelin.kyssion <liyuelin.kyssion@bytedance.com>
2024-02-27 21:48:59 +0800

    faet: add case

--- src/example/playwright_example.ts
+++ src/example/playwright_example.ts
@@ -20,11 +20,40 @@ let run = (async ()=>{
 
     // 传入script脚本获取数据
     let ans = await page.evaluate( ()=>{
+        let ansInfo = new Map<string, any>();
+
         let item =  document.querySelector("#newsListContent")
         if (item==null){
             return null
         }
-        return item.getElementsByClassName("title")[0].innerHTML
+        let childLi = item.getElementsByTagName("li")
+        if (childLi) {
+            for (let li of childLi) {
+                let textDiv = li.getElementsByClassName("text")[0];
+                if (textDiv) {
+                    let title = textDiv.getElementsByClassName("title")[0];
+                    let info = textDiv.getElementsByClassName("info")[0];
+                    let timeNode = textDiv.getElementsByClassName("time")[0];
+                    if (title && info) {
+                        let titleA = title.getElementsByTagName("a")[0];
+                        let infoUrl = titleA.href;
+                        let titleValue = titleA.innerHTML;
+                        let titleInfo = info.getAttribute("title");
+                        let newsTime = timeNode.innerHTML
+                        if (titleValue) {
+                            ansInfo.set(titleValue, {
+                                InfoUrl: infoUrl,
+                                TitleValue: titleValue,
+                                TitleInfo: titleInfo,
+                                Time :newsTime
+                            })
+                            return JSON.stringify(ansInfo)
+                        }
+                    }
+                }
+                return JSON.stringify(ansInfo)
+            }
+        }
     })
     console.log(ans)
     await page.close()
